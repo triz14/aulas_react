6b30604f5d9b6f12e36554940a49a3c67edf33fe
Beatriz <beatriznovais1404@gmail.com>
2023-09-21 14:09:39 -0300

    add icon x on the menu

--- 2_MENU_RESPONSIVO/1_mobile/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/1_mobile/src/components/Header.jsx
@@ -1,18 +1,36 @@
-import { List } from "@phosphor-icons/react"
+import { List, X } from "@phosphor-icons/react"
 import styles from "./Header.module.css"
+import { useState } from "react"
 
 function Header() {
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu() {
+    if(abrirMenu === true) {
+      setAbrirMenu(false)
+      return
+    }
+
+    setAbrirMenu(true)
+  }
+
   return (
     <header className={styles.header}>
         <div className={styles.mobile}>
             <div className={styles.menu}>
-                <button>
-                    <List size={32} />
+                <button onClick = {handleAbrirFecharMenu}>
+                    {
+                      abrirMenu === true ? (
+                        <X size={32}/>
+                    ) : (
+                        <List size={32} />
+                      )
+                    }
                 </button>
 
                 <h1>Logo</h1>
             </div>
-            <div className={styles.options}>
+            <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
               <nav>
                 <a href="">Home</a>
                 <a href="">Resultados</a>
